d54c709575ae7286197e4ea30a1dc8dd2339b631
zleehomc <514300914@qq.com>
2018-01-10 14:51:09 +0800

    2018-01-10

---
+++ test.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+class P{
+
+	public static void main(String[] args) {
+		int[][] a = {{1, 2}, {3, 4, 5}};
+		System.out.println(a.length);
+		//System.out.print(pg.find(k, 2)[0].toString);
+	}
+}
