294c6c57473482d445c014cff26368a41700df36
zleehomc <514300914@qq.com>
2018-01-10 17:13:24 +0800

    1

--- Jump_Game.java
+++ Jump_Game.java
@@ -1,3 +1,13 @@
+/**
+ * @Author: hyc
+ * @Date:   2018-01-05T15:54:25+08:00
+ * @Email:  514300914@qq.com
+ * @Last modified by:   hyc
+ * @Last modified time: 2018-01-10T17:09:09+08:00
+ */
+
+
+
 class Solution {
     public boolean canJump(int[] nums) {
 		int max = 0;
