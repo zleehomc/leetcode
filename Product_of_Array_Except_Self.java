76a5cd2006a9a9e63d8c41b4c288decb12e86f81
zleehomc <514300914@qq.com>
2018-01-11 10:44:00 +0800

    1

--- Product_of_Array_Except_Self.java
+++ Product_of_Array_Except_Self.java
@@ -3,5 +3,23 @@
  * @Date:   2018-01-11T10:23:11+08:00
  * @Email:  514300914@qq.com
  * @Last modified by:   hyc
- * @Last modified time: 2018-01-11T10:23:11+08:00
+ * @Last modified time: 2018-01-11T10:43:54+08:00
  */
+
+
+ class Solution {
+     public int[] productExceptSelf(int[] nums) {
+         int len = nums.length;
+         int right = 1;
+         int[] res = new int[len];
+         res[0] = 1;
+         for(int i = 1; i < len ; i++){
+             res[i] = res[i - 1] * nums[i - 1];
+         }
+         for(int i = len-1 ;i>=0;i--){
+             res[i] = res[i] * right;
+             right = right * nums[i];
+         }
+         return res;
+     }
+ }
